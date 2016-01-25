5db1982e57f07891bdbb9bbeff812c0dadbb18e6
Alex Bauer <aeromusek@gmail.com>
2016-01-24 19:49:56 -0500

    Actually...Examples SHOULD allow a custom title

--- _plugins/example.rb
+++ _plugins/example.rb
@@ -1,5 +1,7 @@
 require 'kramdown'
 
+# Takes one property i.e. {% example title='Here's how you can use this' %}
+
 module Jekyll
     class ExampleTag < Liquid::Block
     def initialize(tag_name, text, tokens)
@@ -15,9 +17,9 @@ module Jekyll
       }
 
       icon = '<i class="fa fa-pencil-square-o"></i> '
-        title = '<h4 class="example__title"> ' + icon + 'Example</h4>'
+      title = if data['title'] then '<h4 class="example__title"> ' + icon + ' Example: ' + data['title'] + '</h4>' else '<h4 class="example__title">' + icon + ' Example</h4>' end
       contents = Kramdown::Document.new(super).to_html
-        '<blockquote class="example">' + title + contents + '</blockquote>'
+      '<blockquote class="example">' + title + contents + '</blockquote>'
     end
   end
 end
