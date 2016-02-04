c072832a2821933855ebba7de9e79fcd35fc7463
Austin Hay <hayaustin@gmail.com>
2016-02-03 20:38:21 -0800

    V1 all Tune adjustments.

--- js/webhook_builders/webhook_builder_tune.js
+++ js/webhook_builders/webhook_builder_tune.js
@@ -8,37 +8,18 @@ function createTuneDynamicDeepLink() {
 	var link = 'http://'
 
 	// Grab Primary Tune Query Parameters
-	var your_advertiser_id = window.document.getElementById('your_advertiser_id').value;
-	var site_id = window.document.getElementById('site_id').value;
 	var advertiser_id = window.document.getElementById('advertiser_id').value;
-	var action = window.document.getElementById('action').value;
-	var site_event_id = window.document.getElementById('site_event_id').value;
-	var site_event_name = window.document.getElementById('site_event_name').value;
+	var site_id = window.document.getElementById('site_id').value;
+	var action = '&action=click';
+	// var site_event_id = window.document.getElementById('site_event_id').value;
+	// var site_event_name = window.document.getElementById('site_event_name').value;
 
 	console.log(action.value)
 	console.log(site_event_id.value)
 	console.log(site_event_name.value)
 
-	// Add Your Tune Advertiser Id to the link ('your_advertiser_id')
-	if (your_advertiser_id.length>0) {
-		link = link + your_advertiser_id;
-	}
-	else {
-		window.alert('Your Tune Advertiser ID is not filled in or is incorrect.');
-		return
-	};
-
 	// Add Tune main string identifiers
-	link = link + '.measure.mobileapptracking.com/serve?sdk=server&response_format=json&ios_ifa={{device.hardware_id}}&google_aid={{device.metadata.google_advertising_id}}&created_at={{event.date}}&user_id={{identity}}&timestamp={{event.date}}&ios_ad_tracking_disabled=0&google_ad_tracking=1'
-
-	// Add Tune Site Id (site_id)
-	if (site_id.length>0) {
-		var link = link + '&site_id=' + site_id;
-	}
-	else {
-		window.alert('Your Tune Site Id is not filled in or is incorrect.');
-		return
-	};
+	link = link + '.measure.mobileapptracking.com/serve?sdk=server&response_format=json&created_at={{event.date}}&user_id={{identity}}&timestamp={{event.date}}&ios_ad_tracking_disabled=0&google_ad_tracking=1&sub_publisher=Branch&sub_campaign={{campaign}}&sub_keyword={{tags}}&tracking_id={{click_id}'
 
 	// Add Tune Advertiser Id (advertiser_id)
 	if (advertiser_id.length>0) {
@@ -49,33 +30,42 @@ function createTuneDynamicDeepLink() {
 		return
 	};
 
-	// Verify at least one Event
-	if (action == "" && site_event_id == "" && site_event_name == "") {
-		window.alert('You must select one event for the postback URL. You cannot leave Predefined Event Name, Site Id and Site Name all blank.')
+	// Add Tune Site Id (site_id)
+	if (site_id.length>0) {
+		var link = link + '&site_id=' + site_id;
+	}
+	else {
+		window.alert('Your Tune Site Id is not filled in or is incorrect.');
 		return
 	};
 
-	// Verify single Event Postback
-	if ((action.length>0 && (site_event_id.length>0 || site_event_name.length>0)) || (site_event_id.length>0 && (action.length>0 || site_event_name.length>0)) || (site_event_name.length>0 && (action.length>0 || site_event_id.length>0))) {
-		window.alert('Only ONE event per postback URL is allowed! Please adjust your settings to include either a Tune Pre-defined Event Name, a Tune Site Event Id, or a Tune Custom Event Id.');
-		return
-	};
+	// // Verify at least one Event
+	// if (action == "" && site_event_id == "" && site_event_name == "") {
+	// 	window.alert('You must select one event for the postback URL. You cannot leave Predefined Event Name, Site Id and Site Name all blank.')
+	// 	return
+	// };
 
-	// Add Tune Predefined Event Name (action)
-	if (action.length>0) {
-		var link = link + '&action=' + action;
-	};
+	// // Verify single Event Postback
+	// if ((action.length>0 && (site_event_id.length>0 || site_event_name.length>0)) || (site_event_id.length>0 && (action.length>0 || site_event_name.length>0)) || (site_event_name.length>0 && (action.length>0 || site_event_id.length>0))) {
+	// 	window.alert('Only ONE event per postback URL is allowed! Please adjust your settings to include either a Tune Pre-defined Event Name, a Tune Site Event Id, or a Tune Custom Event Id.');
+	// 	return
+	// };
 
-	// Add Tune Site Event Id (site_event_id)
-	if (site_event_id.length>0 && action == "" && site_event_name == "") {
-		var link = link + '&action=conversion&site_event_id=' + site_event_id
-		;
-	};
+	// // Add Tune Predefined Event Name (action)
+	// if (action.length>0) {
+	// 	var link = link + '&action=' + action;
+	// };
 
-	// Add Tune Custom Event Id (site_event_name)
-	if (site_event_name.length>0 && action == "" && site_event_id == "") {
-		var link = link + '&action=conversion&site_event_name=' + site_event_name;
-	};
+	// // Add Tune Site Event Id (site_event_id)
+	// if (site_event_id.length>0 && action == "" && site_event_name == "") {
+	// 	var link = link + '&action=conversion&site_event_id=' + site_event_id
+	// 	;
+	// };
+
+	// // Add Tune Custom Event Id (site_event_name)
+	// if (site_event_name.length>0 && action == "" && site_event_id == "") {
+	// 	var link = link + '&action=conversion&site_event_name=' + site_event_name;
+	// };
 
 	// Add Custom Query Parameters
 
--- recipes/webhook_builder_TUNE.md
+++ recipes/webhook_builder_TUNE.md
@@ -6,11 +6,7 @@ hide_platform_selector: true
 
 ## Configuring your Branch + Tune Webhooks ##
 
-The purpose of this link generator is to help you to properly structure Branch + Tune integrated deeplinks.
-
-Want more information on Pre-defined Events? Visit <a href="http://developers.mobileapptracking.com/measure-pre-defined-events/" target="_blank">Tune's Docs on the topic.
-
-Want more information on setting up Custom Events with Tune? Visit <a href="developers.mobileapptracking.com/measuring-custom-events/" target="_blank">Tune's Docs on the topic.
+Use this link builder to create a postback URL, which you can then use to [create a new webhook in your Branch dashboard](https://dashboard.branch.io/#/webhook) to link your Branch and Tune accounts.
 
 {% include webhook-builder-assets/webhook_builder_TUNE.html %}
 
