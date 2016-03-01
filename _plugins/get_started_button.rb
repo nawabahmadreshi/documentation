0b4bb01dfa4f443afb43caa36912d1d01d05cca5
Alex Bauer <aeromusek@gmail.com>
2016-03-01 17:39:47 -0500

    Less hacky approach to the button link

--- _plugins/get_started_button.rb
+++ _plugins/get_started_button.rb
@@ -12,22 +12,15 @@ module Jekyll
 
     def render(context)
       data = {}
-
-      page_url = context.environments.first['page']['url']
-      page_section = context.environments.first['page']['section']
-
-      if page_url.include? page_section then
-        link_path = page_url + '../guide'
-      else
-        link_path = page_url + 'guide'
-      end
+ 
+      link_path = context.environments.first['page']['current_path']
 
       @params.scan(/(\w+)=["']([^'\\]+(\\.[^'\\]+)*)["']/).each { |m|
         data[m[0]] = Liquid::Template.parse(m[1]).render!(context)
       }
 
-      title = if data['title'] then '<a href="' + link_path + '" class="get-started btn btn-primary btn-lg">' + data['title'] + '</a>' else '<a href="' + link_path +'" class="get-started btn btn-primary btn-lg">Get Started!</a>' end
-      title
+      button = if data['title'] then '<a href="/' + link_path + '/guide" class="get-started btn btn-primary btn-lg">' + data['title'] + '</a>' else '<a href="/' + link_path + '/guide" class="get-started btn btn-primary btn-lg">Get Started!</a>' end
+      button
     end
   end
 end
